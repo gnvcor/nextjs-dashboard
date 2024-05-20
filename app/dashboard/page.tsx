2753ace229f53057c932ecab9d3a45bb3bbcd632
Nikita Gordeev <nikita.kolibri@gmail.com>
2024-05-20 11:37:48 +0500

    [Chapter7|fetching-data] Set dashboard page component as async component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 
-export default function Page() {
+export default async function Page() {
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
